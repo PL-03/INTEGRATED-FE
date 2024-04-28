2a68d2dd029eb67bd1d8d1d23f10cc3050ad9681
mattiiee <matboonmalert8@gmail.com>
2024-04-28 23:17:24 +0700

    task modal v1

--- src/components/PopupModal.vue
+++ src/components/PopupModal.vue
@@ -1,38 +1,54 @@
 <script setup>
-import {ref} from "vue"
+import { ref, watch } from "vue"
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+
+dayjs.extend(utc)
+
 const props = defineProps({
-  tasks: {
-    type: Array,
-    // required: true
+  selectedTaskId: {
+    type: Object,
+    required: true,
   },
 })
+
 const isModalOpen = ref(false)
-const closePopup = () =>{
-  emit('closePopup')
+
+watch(
+  () => props.selectedTaskId,
+  (newValue) => {
+    if (Object.keys(newValue).length > 0) {
+      isModalOpen.value = true
+    } else {
+      isModalOpen.value = false
+    }
+  }
+)
+
+const formatDate = (dateString) => {
+  return dayjs.utc(dateString).local().format("DD/MM/YYYY HH:mm:ss")
 }
-console.log(props)
 </script>
 
 <template>
-  <div class="modal">
-      <div  v-for="task in selectedTaskId" :key="task.id" class="modal-content">
-        <p>Task Title: {{ task.taskTitle }}</p>
-        <p>Task Description: {{ task.taskDescription }}</p>
-        <p>Task Assignees: {{ task.taskAssignees }}</p>
-        <p>Task Status: {{ task.taskStatus }}</p>
-        <p>Task Created Date: {{ task.createdOn }}</p>
-        <p>Task Updated Date: {{ task.createdOn }}</p>  
-      </div>
+  <div v-if="isModalOpen" class="modal">
+    <div class="modal-content">
+      <span class="close" @click="isModalOpen = false">&times;</span>
+      <h2>Task Details</h2>
+      <p>Task Title: {{ selectedTaskId.taskTitle }}</p>
+      <p>Task Description: {{ selectedTaskId.taskDescription }}</p>
+      <p>Task Assignees: {{ selectedTaskId.taskAssignees }}</p>
+      <p>Task Status: {{ selectedTaskId.taskStatus }}</p>
+      <p>Task Created Date: {{ formatDate(selectedTaskId.createdOn) }}</p>
+      <p>Task Updated Date: {{ formatDate(selectedTaskId.updatedOn) }}</p>
+    </div>
   </div>
-
-
-
 </template>
 
 <style scoped>
 /* Styles for modal and overlay */
 .modal {
-  display: none; /* Hidden by default */
+  display: block; /* Show the modal */
   position: fixed; /* Stay in place */
   z-index: 1; /* Sit on top */
   padding-top: 100px; /* Location of the box */
@@ -41,8 +57,8 @@ console.log(props)
   width: 100%; /* Full width */
   height: 100%; /* Full height */
   overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
 }
 
 /* Modal Content */
