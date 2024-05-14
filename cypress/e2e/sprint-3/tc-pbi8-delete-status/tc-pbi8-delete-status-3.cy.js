e7c402063be6f52c422e34ab4f47509a1d73be90
Threepoom <threepoom.ngam@kmutt.ac.th>
2024-05-14 21:50:17 +0700

    add cypress

---
+++ cypress/e2e/sprint-3/tc-pbi8-delete-status/tc-pbi8-delete-status-3.cy.js
@@ -0,0 +1,14 @@
+describe(`TC-PBI8-DELETE-STATUS-3\n 
+          Test Scenario : normal - delete status with task(s) \n
+           - transfer task(s) to another status.`, () => {
+  
+    beforeEach(()=> {
+        cy.viewport(1024, 768)
+        cy.visit('/task') ;
+        cy.wait(100) ;
+    }) ;
+    
+    it('Run this test with your advisor.', () => {
+    
+    })
+})
