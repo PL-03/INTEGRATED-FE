2b22808dfb8e098b667212c05ddd965cbbffc8f1
Pimpajee <pimpajee.seth@mail.kmutt.ac.th>
2024-05-20 23:02:19 +0700

    ui sort filter1

--- src/components/FilterDropdown.vue
+++ src/components/FilterDropdown.vue
@@ -45,18 +45,18 @@ const filter = () => {
 </script>
 
 <template>
-  <div class="itbkk-status-filter relative inline-flex">
+  <div class="itbkk-status-filter relative ">
+    <div class="flex ml-28">
     <div
-      class="flex w-96  h-11 px-4 bg-white rounded-md border-gray-300 rounded-l focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-400 overflow-x-scroll"
-      >
-      <div class="m-2 mr-2 whitespace-nowrap text-gray-600 text-sm  "><strong>Filter by status</strong></div>
-      
+      class="flex w-72  h-11 px-4 bg-white rounded-md border-gray-300 rounded-l focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-400 overflow-x-scroll">
+      <div class="itbkk-filter-item mt-2 mb-1 whitespace-nowrap text-gray-500 text-sm  "><strong class="">Filter by status :</strong></div>
+
       <div v-for="(option, index) in selectedOptions" :key="index"
-        class=" flex items-center text-sm  bg-gray-200 rounded-full px-4 py-3 mt-2 text-sm whitespace-nowrap ">
+        class="itbkk-filter-item  m-2 flex items-center text-sm  bg-gray-300 rounded-full px-3 py-3 mt-2 text-sm whitespace-nowrap ">
         <span>{{ option }}</span>
-        <button type="button" class="ml-1  text-gray-500 hover:text-red-700 focus:outline-none"
+        <button type="button" class="itbkk-filter-item ml-1  text-gray-500 hover:text-red-700 focus:outline-none"
           @click="removeOption(index)">
-          <svg class="h-3 w-3 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <svg class="itbkk-filter-item-clear h-3 w-3 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
             <path
               d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
           </svg>
@@ -67,42 +67,25 @@ const filter = () => {
     <div class="relative">
       <button
         class="h-11 px-4 py-2 bg-blue-500 text-white rounded-l-none hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        @click="toggleDropdown"
-      >
-        <svg
-          class="h-4 w-4 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path
-            d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-          />
+        @click="toggleDropdown">
+        <svg class="h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
         </svg>
       </button>
-      <div
-        v-if="showDropdown"
-        class="absolute left-0 z-10 mt-2 bg-white rounded-lg shadow-lg"
-        @click="filter"
-      >
+      <div v-if="showDropdown" class="absolute left-0 z-10 mt-2 bg-white rounded-lg shadow-lg" @click="filter">
         <ul class="py-2 text-sm">
-          <li
-            v-for="(status, index) in statuses"
-            :key="index"
-            class="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-            @click.stop="addOption(status)"
-          >
+          <li v-for="(status, index) in statuses" :key="index" class="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+            @click.stop="addOption(status)">
             {{ status }}
           </li>
         </ul>
       </div>
     </div>
-    <button
-      v-if="selectedOptions.length > 0"
-      type="button"
-      class="ml-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
-      @click="clearOptions"
-    >
+    <button v-if="selectedOptions.length > 0" type="button"
+      class="itbkk-filter-clear ml-2 mt-3 h-8 px-3 py-1 bg-red-600 text-white rounded hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+      @click="clearOptions">
       Clear All
     </button>
+  </div>
   </div>
 </template>
--- src/components/TaskTable.vue
+++ src/components/TaskTable.vue
@@ -208,12 +208,12 @@ const showToast = (message, type) => {
       </div>
     </nav>
     <div class="p-28 ">
-      <div class="filter mb-2 ">
+      <div class="itbkk-status-filter mb-2 br">
         <FilterDropdown :tasks="tasks" @filter="handleFilterData" />
       </div>
 
       <div class="flex justify-center items-center ">
-        <table class="table-auto w-9/12  m-2 rounded-3xl overflow-hidden">
+        <table class="table-auto w-10/12  m-2 rounded-3xl overflow-hidden">
           <thead class="bg-yellow-950 border-b py-4 text-white">
             <tr>
               <th class="text-lg font-medium text-white px-4 py-2 text-left border-r">
@@ -228,10 +228,11 @@ const showToast = (message, type) => {
               <th class="flex text-lg font-medium text-white px-4 py-2 text-left border-r">
                 Status
 
-                <div class="rounded-md flex bg-white px-1 py-1 ml-4">
+                <div class="itbkk-status-sort rounded-md flex bg-white px-1 py-1 ml-4">
                   <button
                     class=" ml-1 rounded-md px-1 py-1 bg-white text-yellow-950 focus:bg-yellow-200 hover:bg-yellow-200 hover:text-black"
-                    @click="handleSortData('Asc')"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                    @click="handleSortData('Asc')">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
                       viewBox="0 0 24 24">
                       <path fill="currentColor"
                         d="M19.707 14.707A1 1 0 0 0 19 13h-7v2h4.586l-4.293 4.293A1 1 0 0 0 13 21h7v-2h-4.586zM7 3.99H5v12H2l4 4l4-4H7zM17 3h-2c-.417 0-.79.259-.937.649l-2.75 7.333h2.137L14.193 9h3.613l.743 1.981h2.137l-2.75-7.333A1 1 0 0 0 17 3m-2.057 4l.75-2h.613l.75 2z" />
