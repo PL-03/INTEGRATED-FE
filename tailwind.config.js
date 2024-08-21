cff5ef473a53b41b9dbc82b90c72dc0cfb8bdce2
Pimpajee <pimpajee.seth@mail.kmutt.ac.th>
2024-08-21 15:16:30 +0700

    add:ui all page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lilita: [
+          '"Lilita One"',
+          ...fontFamily.sans,
+        ],
+      },
+    },
   },
   plugins: [],
 }
