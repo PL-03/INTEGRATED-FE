31787e0b8a8f1ade2cf5033c09967f49773c66f6
Threepoom <threepoom.ngam@kmutt.ac.th>
2024-12-09 22:00:10 +0700

    add go back btn

--- src/components/AccessDenied.vue
+++ src/components/AccessDenied.vue
@@ -1,11 +1,26 @@
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+const goBack = () => {
+  router.push({ name: "boardslist" });
+};
+</script>
 
 <template>
-  <div class="bg-[#45474c] min-h-screen font-lilita flex flex-col items-center gap-6">
-    <img src="../assets/access-denied.png" alt="Access Denied" width="96" height="96" class="mt-20"/>
+  <div
+    class="bg-[#45474c] min-h-screen font-lilita flex flex-col items-center gap-6"
+  >
+    <img
+      src="../assets/access-denied.png"
+      alt="Access Denied"
+      width="96"
+      height="96"
+      class="mt-20"
+    />
     <h1 class="text-xl text-white">
       Access Denied, you don't have permission to view this page.
     </h1>
+    <button @click="goBack">Go Back</button>
   </div>
 </template>
 
